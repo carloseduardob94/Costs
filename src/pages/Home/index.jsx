ece697c4a4ad78d6b34f58cab136080a60b0a705
Eduardo <educonts08@gmail.com>
2022-09-08 21:23:39 -0300

    Home done

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,14 @@
+import { Container } from './styles'
+import savings from '../../img/savings.svg'
+import { LinkButton } from '../../components/LinkButton'
+
 export const Home = () => {
-  return <h1>Home</h1>
+  return(
+    <Container>
+      <h1>Bem-vindo ao <span>Costs</span></h1>
+      <p>Comece a gerenciar os seus projetos agora mesmo!</p>
+      <LinkButton to="/newproject" text="Criar Projetos"/>
+      <img src={savings} alt="Costs" />
+    </Container>
+  )
 }
